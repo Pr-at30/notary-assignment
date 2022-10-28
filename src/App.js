9dd63b1d5be3137933d509357b2db04fae27915f
Pratyush Jena <pratyushjena8@gmail.com>
2022-10-29 01:28:57 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  const handleClick = () => {
+
+    axios.post('http://demo2211087.mockable.io/mock', {
+      body: {}
+    })
+      .then(function (response) {
+        setData(response.data.companies);
+      })
+      .catch(function (error) {
+        console.log(error);
+      }
+      );
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container main-container'>
+        <h1>The Notary App</h1>
+        {
+          !data ? <button className='btn btn-primary butt' onClick={handleClick}>Show data</button>
+            :
+            <table className="table table-dark table-hover table--edit">
+              <thead className='head-edit'>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((item) => (
+                  <tr key={item.name}>
+                    <td className="col ">{item.name}</td>
+                    <td className="col ">{item.email}</td>
+                    <td className="col ">{item.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+        }
+      </div>
+    </>
   );
 }
+
 
 export default App;
